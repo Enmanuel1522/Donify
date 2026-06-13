85fbb1cdc62e66a225be2997cd6fed0043706ba8
Enmanuel <enmanueldemejia1@gmail.com>
2026-06-12 21:56:58 -0400

    Creation of the Donation entity

---
+++ Donify/Donify.API/Models/Entities/Donation.cs
@@ -0,0 +1,22 @@
+﻿namespace Donify.API.Models.Entities
+{
+    public class Donation
+    {
+        public int Id { get; set; }
+
+        public int DonorId { get; set; }
+        public Donor? Donor { get; set; }
+
+        public decimal Amount { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime DonatedAt { get; set; } = DateTime.UtcNow;
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string Status { get; set; } = "Pending";
+        public string? ReceiptUrl { get; set; }
+
+    }
+}
+
+
+
